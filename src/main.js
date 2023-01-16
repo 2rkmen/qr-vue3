752c425c65321da893c61e6b1cee1ddfeca5a366
akurbanov <maotsedun@mail.ru>
2023-01-16 18:51:08 +0300

    vue3-qrcode-reader

--- src/main.js
+++ src/main.js
@@ -6,11 +6,8 @@ import router from './router'
 
 import './assets/main.css'
 
-import VueQrcodeReader from "vue-qrcode-reader";
-
 const app = createApp(App)
 
-app.use(VueQrcodeReader);
 app.use(createPinia())
 app.use(router)
 
