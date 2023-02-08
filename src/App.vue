7aa76bc6d220ad5a09d41242613e854d96481f44
akurbanov <maotsedun@mail.ru>
2023-02-08 16:13:56 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it,deployed" />
+      <HelloWorld msg="You did it, dist" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
